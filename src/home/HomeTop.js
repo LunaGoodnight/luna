5b05f3352b19ec3de99ef09a8ec4da3007f89eed
Luna <javaspringtea@gmail.com>
2025-07-05 17:59:31 +0800

    rm quote api

--- src/home/HomeTop.js
+++ src/home/HomeTop.js
@@ -36,6 +36,19 @@ const WrapImage = styled.div`
 `;
 const ImagePart = styled.div``;
 
+const quoteList = [
+  "The only way to do great work is to love what you do.",
+  "Believe you can and you're halfway there.",
+  "The future belongs to those who believe in the beauty of their dreams.",
+  "It's not whether you get knocked down, it's whether you get up.",
+  "The best way to predict the future is to create it.",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "Your time is limited, don't waste it living someone else's life.",
+  "The only limit to our realization of tomorrow will be our doubts of today.",
+  "Do not wait for a leader; do it alone, person to person.",
+  "The mind is everything. What you think you become."
+]
+
 const getQuote = (data) => {
   if (data) {
     return data[getRandomInt(data.length)].text;
@@ -43,10 +56,7 @@ const getQuote = (data) => {
   return null;
 };
 export const HomeTop = () => {
-  const { data } = useQuery("quotes", () =>
-    fetch("https://type.fit/api/quotes").then((res) => res.json())
-  );
-  const cacheData = useMemo(() => getQuote(data), [data]);
+
 
   return (
     <Wrapper>
@@ -55,7 +65,7 @@ export const HomeTop = () => {
           <WrapImage>
             <img src="https://picsum.photos/600/200" alt="picsum" />
           </WrapImage>
-          <QuoteText>{data ? cacheData : null}</QuoteText>
+          <QuoteText>{getQuote(quoteList)}</QuoteText>
         </ImagePart>
       </HomeTopStyle>
     </Wrapper>
